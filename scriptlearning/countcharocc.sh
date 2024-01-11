f0bb772b8ab91901b05d1df27e33fd1d8459cff4
sanjay0288 <87768165+sanjay0288@users.noreply.github.com>
2024-01-11 07:47:00 +0530

    Update countcharocc.sh

--- scriptlearning/countcharocc.sh
+++ scriptlearning/countcharocc.sh
@@ -3,4 +3,4 @@ stri=$1
 chtofind=$2
 occ=$(echo "$stri" | grep -o "$chtofind" | wc -l)
 echo "$chtofind is repeated $occ times in the string $stri"
-echo"Added this line for poll SCM"
+echo "Added this line for poll SCM"
